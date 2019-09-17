7742a12832ba9b15e9e9aaede39aea28c557e0f6
JosefWalls <josef.l.walls@gmail.com>
2019-09-17 14:20:39 -0500

    Completed the practice

---
+++ javascript/Notes.js
@@ -0,0 +1,11 @@
+take one light out and replace it, turn it on and if it dont work, then its the other one
+
+var lightOne = prompt("Is light one broken");
+var lightTwo = prompt("Is light two broken");
+
+function lit () {
+    if (lightOne !== On;)
+    alert("The first is broken");
+} else {
+    alert("The second is broken");
+}
